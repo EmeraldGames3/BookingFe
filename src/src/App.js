b02ab42f63335ff395080508eb5e61225d443391
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2024-03-16 02:01:44 +0200

    Axios works

--- src/src/App.js
+++ src/src/App.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
 import LoginSignUp from './Components/WelcomePage/LoginSignUp';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+})
 
 function App() {
+  let error = null;
+  let response = null;
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}`).then(response => {
+      console.log(response.data);
+    }).catch(error => { console.log(error) });
+  }, []);
+  
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
+        <div>{response}</div>
         <LoginSignUp />
     </div>
   );
